c9d43284dd5135c56f4700e07520dedbc8f95beb
kmiller <kmmiller418@gmail.com>
2022-06-01 11:32:50 -0400

    Create OrderController and routes

---
+++ src/main/java/payroll/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package payroll;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(Long id) {
+        super("Order not found " + id);
+    }
+}
--- src/main/java/payroll/OrderRepository.java
+++ src/main/java/payroll/OrderRepository.java
@@ -3,4 +3,5 @@ package payroll;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
