da92666a8d3fc7bd9c69cb074cdeb65fac5f9e7c
kmiller <kmmiller418@gmail.com>
2022-06-01 11:46:32 -0400

    Seed database with test orders

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -11,10 +11,12 @@ class LoadDatabase {
     public static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository repository, OrderRepository oRepo) {
         return args -> {
             log.info("Preloading " + repository.save(new Employee("Test", "Testerson", "tester")));
             log.info("Preloading " + repository.save(new Employee("Tess", "Tester", "test writer")));
+            log.info("Preloading " + oRepo.save(new Order("Clipboard", Status.IN_PROGRESS)));
+            log.info("Preloading " + oRepo.save(new Order("Fake mustache", Status.COMPLETED)));
         };
     }
 
