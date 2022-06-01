6d9339ccedd644c93906715e35c6b33a9c5092e0
kmiller <kmmiller418@gmail.com>
2022-06-01 09:13:52 -0400

    Create GET routes and EmployeeNotFoundException

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -1,7 +1,27 @@
 package payroll;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 class EmployeeController {
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
 }
